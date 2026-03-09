3ee66577220b97f6c6b2c3eb2da9448a5aab689c
Pavel Postolnyi <pavlopostolny@gmail.com>
2026-03-09 13:15:50 +0200

    made code refactoring + made method for calculating leaderboard (this method needs to be tested)

--- dispatchers/evaluation.py
+++ dispatchers/evaluation.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 from enum import IntFlag
-from submission import Submission
+from typing import List
 from account import User
+from leaderboard import LeaderboardEntry
+from submission import Submission
 
 
 class EvaluationCriterion(IntFlag):
@@ -40,3 +43,27 @@ class Evaluation:
 
     async def calculate_total(self) -> float:
         pass
+
+    def calculate_leaderboard(self, evaluations: List) -> list[LeaderboardEntry]:
+        # TODO: needs to be tested
+
+        scores = defaultdict(list)
+
+        for eval in evaluations:
+            scores[eval.team_id].append(eval.score)
+
+        leaderboard = []
+
+        for team_id, team_scores in scores.items():
+            total = sum(team_scores)
+            average = total / len(team_scores)
+            leaderboard.append(
+                LeaderboardEntry(
+                    team_id=team_id,
+                    total_score=total,
+                    average_score=average
+                )
+            )
+
+        leaderboard.sort(key=lambda x: x.total_score, reverse=True)
+        return leaderboard
