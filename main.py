eb7fe1e2f9436098f8b15f6490ce36ae8fb42443
Pavel Postolnyi <pavlopostolny@gmail.com>
2026-03-09 10:18:52 +0200

    made basic websocket server with error handling

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import uvicorn
+
+from dispatchers.connection import Connection
+
+app = FastAPI()
+
+
+manager = Connection()
+
+
+@app.websocket("/apiws")
+async def websocket_endpoint(websocket: WebSocket):
+
+    await manager.connect(websocket)
+    try:
+        while True:
+            await manager.receive_message(websocket)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
+uvicorn.run(app, host="0.0.0.0", port=8000)
