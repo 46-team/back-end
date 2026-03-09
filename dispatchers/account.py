996c31db44a47bda6a6f66e2b61a6f374a6145a8
Pavel Postolnyi <pavlopostolny@gmail.com>
2026-03-09 11:59:05 +0200

    added account classes

---
+++ dispatchers/account.py
@@ -0,0 +1,32 @@
+from enum import IntFlag
+import uuid
+import time
+
+
+class UserRole(IntFlag):
+    NONE = 0
+    IS_ADMIN = 1
+    IS_ORGANIZER = 2
+    IS_TEAM = 4
+    IS_JURY = 8
+
+
+class User:
+    def __init__(self, user_id: str, name: str, email: str, role: UserRole, created_at: int):
+        self.id = user_id
+        self.name = name
+        self.email = email
+        self.role = role
+        self.created_at = created_at
+
+    async def change_role(self, new_role: UserRole):
+        self.role = new_role
+
+    def add_role(self, role: UserRole):
+        self.role |= role
+
+    def remove_role(self, role: UserRole):
+        self.role &= ~role
+
+    def has_role(self, role: UserRole) -> bool:
+        return bool(self.role & role)
