bf62aa7d48c371ef98e0c9a85217eeae72bd7753
Cot_Lot <cattemeo@gmail.com>
2026-03-26 01:13:39 +0200

    i made login and register account

---
+++ dispatchers/login_account.py
@@ -0,0 +1,48 @@
+import uuid
+import time
+from account import User, UserRole
+from register_account import USER_DB, SESSION
+
+class login_account:
+    @staticmethod
+    async def login_account(data: dict):
+        email = data.get("email")
+        password = data.get("password")
+        device_token = data.get("device_token")
+
+        if not email or not password or not device_token:
+            return {
+                "is_ok": False,
+                "error_code": "#MISSING_FIELDS",
+                "error_message": "Email, password or device_token is important"
+            }
+
+        
+        user_record = USER_DB.get(email)
+
+        if not user_record or user_record["password"] != password:
+            return {
+                "is_ok": False,
+                "error_code": "#AUTH_FAILED",
+                "error_message": "Wrong password or email"
+            }
+
+        user = user_record["user"]
+
+        
+        SESSION[device_token] = user.id
+
+        return {
+            "is_ok": True,
+            "type": "login-account",
+            "data": {
+                "user": {
+                    "id": user.id,
+                    "name": user.name,
+                    "email": user.email,
+                    "role": int(user.role)
+                },
+                "device_token": device_token,
+                "status": "logged_in"
+            }
+        }
