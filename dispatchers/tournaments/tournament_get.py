a163a99ccaf85cace545f8d672adfb50f9f3a790
Cot_Lot <cattemeo@gmail.com>
2026-04-29 21:52:03 +0300

    fixs

--- dispatchers/tournaments/tournament_get.py
+++ dispatchers/tournaments/tournament_get.py
@@ -1,8 +1,16 @@
 from fastapi import WebSocket
 from bson import ObjectId
+from services.tournament_service import serialize_mongo_document
 from bson.errors import InvalidId
 import dispatchers.utils.FGProto as FGProto
-from dispatchers.utils.error_templates import err_incompl_request, err_invalid_id, err_not_found
+from dispatchers.utils.error_templates import (
+    err_incompl_request,
+    err_invalid_id,
+    err_not_found,
+    err_empty_token,
+    err_invalid_token,
+)
+
 
 TOURNAMENT_FIELDS = {
     "_id": 1,
@@ -15,40 +23,35 @@ TOURNAMENT_FIELDS = {
     "created_at": 1,
 }
 
-def _fmt(doc: dict) -> dict:
-    doc["_id"] = str(doc["_id"])
-    if doc.get("created_by"):
-        doc["created_by"] = str(doc["created_by"])
-    return doc
-
-
-async def get_tournaments(
-    client: WebSocket, message: dict, db: any,
-    proto: FGProto, ENCRYPTION_KEYS: dict
-) -> None:
-    cursor = db["tournaments"].find({}, TOURNAMENT_FIELDS)
-    tournaments = [_fmt(doc) async for doc in cursor]
-
-    await proto.send_message(
-        {
-          "is_ok": True,
-          "type": "get_tournaments", 
-          "tournaments": tournaments
-        },
-        ENCRYPTION_KEYS[client]["key"],
-    )
-
 
 async def get_tournament(
-    client: WebSocket, message: dict, db: any,
-    proto: FGProto, ENCRYPTION_KEYS: dict
+    client: WebSocket, 
+    message: dict, 
+    db: any,
+    USER_TOKENS: dict, 
+    proto: FGProto, 
+    ENCRYPTION_KEYS: dict
 ) -> None:
-    if not message.get("tournament_id"):
-        await err_incompl_request(
+    if not isinstance(message.get("token"), str):
+        await err_empty_token(
             proto=proto, 
             ENCRYPTION_KEYS=ENCRYPTION_KEYS, 
             client=client
             )
+        return
+
+    if not (message["token"] in USER_TOKENS and USER_TOKENS[message["token"]][0] == client):
+        await err_invalid_token(
+            proto=proto, 
+            ENCRYPTION_KEYS=ENCRYPTION_KEYS, 
+            client=client)
+        return
+
+    if not message.get("tournament_id"):
+        await err_incompl_request(
+            proto=proto, 
+            ENCRYPTION_KEYS=ENCRYPTION_KEYS, 
+            client=client)
         return
 
     try:
@@ -57,8 +60,7 @@ async def get_tournament(
         await err_invalid_id(
             proto=proto, 
             ENCRYPTION_KEYS=ENCRYPTION_KEYS, 
-            client=client
-            )
+            client=client)
         return
 
     doc = await db["tournaments"].find_one({"_id": oid}, TOURNAMENT_FIELDS)
@@ -68,15 +70,13 @@ async def get_tournament(
             proto=proto, 
             ENCRYPTION_KEYS=ENCRYPTION_KEYS, 
             client=client, 
-            entity="tournaments"
-            )
+            entity="tournament")
         return
 
     await proto.send_message(
         {
             "is_ok": True, 
             "type": "get_tournament", 
-            "tournament": _fmt(doc)
-        },
+            "tournament": serialize_mongo_document(doc)},
         ENCRYPTION_KEYS[client]["key"],
     )
